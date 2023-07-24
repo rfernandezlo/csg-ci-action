7bc20275711963634f07cd2b8417c6029dbeba3d
rfernandezlopez <rfernandezlopez@salesforce.com>
2023-07-24 03:03:44 +0200

    New

--- dist/index.js
+++ dist/index.js
@@ -9829,7 +9829,7 @@ const main =   async () => {
         try {
             core.debug(`Parsing inputs`);
             const r_status = core.getInput('status');
-            const r_token = core.getInput('repo-token', { required: true });
+            const r_token = core.getInput('repo_token', { required: true });
             const r_name = core.getInput('name');
             const r_conclusion = core.getInput('conclusion');
             const octokit = new github.getOctokit(r_token);
@@ -9837,11 +9837,11 @@ const main =   async () => {
             core.debug(`Creating a new Run on ${r_status}/${r_name}@${r_token}`);
             const ownership = {
                 owner: github.context.repo.owner,
-                repo: github.context.repo.repo,
+                repo: core.getInput('repo'),
             };
             const output = {
-                title: core.getInput('output-title'),
-                summary: core.getInput('output-summary'),
+                title: core.getInput('output_title'),
+                summary: core.getInput('output_summary'),
             };
             await octokit.rest.checks.create({
                 ...ownership,
