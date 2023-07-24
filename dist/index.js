fc7d3f07d4598ec4ee902e38648da27111963f2b
rfernandezlopez <rfernandezlopez@salesforce.com>
2023-07-24 02:51:11 +0200

    sa

--- dist/index.js
+++ dist/index.js
@@ -9825,30 +9825,31 @@ async function checkFileStartsWithHeader(filePath) {
     });
 }
 
-(
-    async () => {
+const main =   async () => {
         try {
             core.debug(`Parsing inputs`);
             const r_status = core.getInput('status');
             const r_token = core.getInput('repo-token', { required: true });
             const r_name = core.getInput('name');
             const r_conclusion = core.getInput('conclusion');
-            const r_title = core.getInput('output-title');
-            const r_summary = core.getInput('output-summary');
             const octokit = new github.getOctokit(r_token);
             core.debug(`Setting up OctoKit`);
             core.debug(`Creating a new Run on ${r_status}/${r_name}@${r_token}`);
-            await octokit.rest.checks.create({
+            const ownership = {
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
+            };
+            const output = {
+                title: core.getInput('output-title'),
+                summary: core.getInput('output-summary'),
+            };
+            await octokit.rest.checks.create({
+                ...ownership,
                 name: r_name,
                 head_sha: github.context.sha,
                 status: r_status,
                 conclusion: r_conclusion,
-                output: {
-                    title: r_title,
-                    summary: r_summary
-                }
+                ...output,
             });
             core.debug(`Done`);
         } catch (error) {
@@ -9856,7 +9857,7 @@ async function checkFileStartsWithHeader(filePath) {
         }
     }
 
-)();
+main();
 })();
 
 module.exports = __webpack_exports__;
